fe3abb171e007a34753956ac3630c1194019144e
ernst <rfanfan@gmail.com>
2020-11-01 21:06:14 -0500

    Added BP calculator function into person.py and added variables for sp and dp

--- person.py
+++ person.py
@@ -24,6 +24,8 @@ class Person:
         self.freqs = []              # frequencies in bpm
         self._firstTime = 0.0
         self._index = 0
+        self.sp = array('d')
+        self.dp = array('d')
 
     def setFace(self, face):
         """
@@ -78,8 +80,28 @@ class Person:
                 p = int(0.5 + conf.AV_BPM_PERIOD * fps)
                 if len(self.bpm) == conf.MAX_SAMPLES and not self._index % p:
                     av = np.average(self.bpm[-p:])
+                    sp, dp = self._blood_preasure_calculator(av,330,73,39)
+                    self.sp.append(sp)
+                    self.dp.append(dp)
                     self.avBpm.append(av)
 
+    def _blood_preasure_calculator(self, avg_bpm, weight, height, age):
+
+        kgs = weight * 0.45359237  # lbs to kgs
+        cm = height / 0.39370  # in to cm
+        q = 4.5  # constant
+
+        rob = 18.5
+        et = (364.5 - 1.23 * avg_bpm)
+        bsa = 0.007184 * (kgs ** 0.425) * (cm ** 0.725)
+        sv = (-6.6 + (0.25 * (et - 35)) - (0.62 * avg_bpm) + (40.4 * bsa) - (0.51 * age))
+        pp = sv / ((0.013 * kgs - 0.007 * age - 0.004 * avg_bpm) + 1.307)
+        mpp = q * rob
+
+        sp = int(mpp + 3 / 2 * pp)
+        dp = int(mpp - pp / 3)
+
+        return sp, dp
     def _getSignal(self, greenIm, face):
         """
         Acquire a signal sample by averaging over a ROI in the green channel.
