a7f3ce0f53e204838ee74c7cb21de05ba41a8b54
GIGA-Money <gigachris1@hotmail.com>
2020-11-02 16:44:43 -0500

    readded the q input dialog for inputing params for weight, height, age

--- pulse.py
+++ pulse.py
@@ -24,12 +24,16 @@ class Window(qt.QMainWindow):
         self.setCentralWidget(self.view)
         self.curves = CurveWidget_bp()
         self.curves.show()
+        self.pheight = 0.0
+        self.pweight = 0.0
+        self.age = 0
 
         def addAction(menu, name, shortcut, cb):
             action = qt.QAction(name, self)
             action.setShortcut(shortcut)
             action.triggered.connect(cb)
             menu.addAction(action)
+
         menu = self.menuBar()
         fileMenu = menu.addMenu('Source')
         camMenu = fileMenu.addMenu('Camera')
@@ -85,10 +89,17 @@ class Window(qt.QMainWindow):
     def stop(self):
         self.video.stop()
 
+    def set_boiler_plate(self, weight, height, age):
+        self.pheight = height
+        self.pweight = weight
+        self.age = age
+
     async def pipeline(self):
         self.video = VideoStream(conf.CAM_ID)
         scene = self.video | FaceTracker | SceneAnalyzer
         lastScene = scene.aiter(skip_to_last=True)
+        _, person = lastScene
+        person.set_boiler_plate(self.pweight, self.pheight, self.age)
         async for frame, persons in lastScene:
             self.view.draw(frame.image, persons)
             if self.curves.isVisible():
@@ -99,6 +110,12 @@ def pulse():
     if len(sys.argv) > 1:
         conf.CAM_ID = sys.argv[1]
     qApp = qt.QApplication(sys.argv)  # noqa
+    weight, ok = qt.QInputDialog.getDouble(None, "Insert Weight", """weight in pounds""")
+    height, ok = qt.QInputDialog.getDouble(None, "Insert Height", """height """)
+    age, ok = qt.QInputDialog.getInt(None, "insert your age", """age testing:""")
+    if not ok:
+        sys.exit(qApp.exec_())
     win = Window()
+    win.set_boiler_plate(weight=weight, height=height, age=age)
     win.show()
     util.run()
