0f5978325b3946cd0c80c1c8dff6870e630bda5d
ernst <rfanfan@gmail.com>
2020-11-02 10:16:51 -0500

    Added .draw overwrite. Changed applet to use average Bpm instead of raw Bpm to stabilize output

--- widgets.py
+++ widgets.py
@@ -1,10 +1,31 @@
 from heartwave.widgets import View, CurveWidget
 from heartwave.plot import Plot
+import heartwave.util as util
+import PyQt5.Qt as qt
 
 
 class View_us(View):
     def __init__(self, parent):
             super().__init__(parent)
+
+    def draw(self, im, persons):
+        qim = util.qImage(im)
+        with qt.QPainter(qim) as p:
+            for person in persons:
+                x, y, w, h = person.face
+                p.setPen(qt.QColor(255, 255, 255, 64))
+                p.drawRect(x, y, w, h / 4)
+                p.drawRect(x, y + h / 2, w, h / 4)
+                font = p.font()
+                font.setPixelSize(28)
+                p.setFont(font)
+                p.setPen(qt.QColor(255, 255, 255))
+                bpm = person.avBpm[-1] if len(person.avBpm) else 0
+                p.drawText(
+                    x, y, w, h, qt.Qt.AlignHCenter, '♡' + str(int(bpm)))
+        self.image = qim
+        self.setMinimumSize(qim.size())
+        self.update()
 
 
 class CurveWidget_bp(CurveWidget):
@@ -23,5 +44,7 @@ class CurveWidget_bp(CurveWidget):
         sp, dp = self.bp_plots
         for person in persons:
             sp.plot(person.sp)
+            sp.plot(person.avg_sp, pen=qt.Qt.red)
         for person in persons:
             dp.plot(person.dp)
+            dp.plot(person.avg_dp, pen=qt.Qt.red)
